cfe8592ae3e1d118a20b7509ce0784db5e14b2f6
nedhmn <hermannned@gmail.com>
2024-12-15 06:41:53 -0500

    fix: remove box

--- packages/utils/aesthetics.R
+++ packages/utils/aesthetics.R
@@ -12,32 +12,27 @@
 #'
 #' @export
 nedhmn_theme <- function(show_legend = FALSE) {
-    # Dependencies
-    box::use(
-        ggplot2[element_blank, element_line, element_rect, element_text, margin, theme],
-    )
-
-    theme(
-        axis.text = element_text(color = "#d1d5db", size = 9),
-        axis.text.x = element_text(margin = margin(5, 0, 5, 0)),
-        axis.text.y = element_text(margin = margin(0, 5, 0, 5)),
-        axis.ticks = element_line(color = "#fff", linewidth = 0.1),
-        axis.title = element_text(color = "#fff", size = 10),
-        legend.background = element_blank(),
+    ggplot2::theme(
+        axis.text = ggplot2::element_text(color = "#d1d5db", size = 9),
+        axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, 0, 5, 0)),
+        axis.text.y = ggplot2::element_text(margin = ggplot2::margin(0, 5, 0, 5)),
+        axis.ticks = ggplot2::element_line(color = "#fff", linewidth = 0.1),
+        axis.title = ggplot2::element_text(color = "#fff", size = 10),
+        legend.background = ggplot2::element_blank(),
         legend.position = if (show_legend) "right" else "none",
-        legend.text = element_text(color = "#d1d5db"),
-        legend.title = element_text(color = "#fff"),
-        panel.background = element_blank(),
-        panel.border = element_rect(fill = NA, color = "#e5e7eb91", linewidth = 0.2),
-        panel.grid.major = element_line(color = "#e5e7eb54", linetype = 2, linewidth = 0.1),
-        panel.grid.minor = element_line(color = "#e5e7eb54", linetype = 2, linewidth = 0.1),
-        plot.background = element_rect(fill = "#030712", linewidth = 0),
-        plot.caption = element_text(color = "#d1d5db", size = 8),
-        plot.margin = margin(5, 15, 5, 10),
-        plot.subtitle = element_text(color = "#d1d5db", size = 10, margin = margin(0, 0, 10, 0)),
-        plot.title = element_text(color = "#fff", face = "bold", size = 14, margin = margin(5, 0, 5, 0)),
-        strip.background = element_rect(fill = "#1d1f27", color = "#e5e7eb91", linewidth = 0.1),
-        strip.text = element_text(color = "#fff")
+        legend.text = ggplot2::element_text(color = "#d1d5db"),
+        legend.title = ggplot2::element_text(color = "#fff"),
+        panel.background = ggplot2::element_blank(),
+        panel.border = ggplot2::element_rect(fill = NA, color = "#e5e7eb91", linewidth = 0.2),
+        panel.grid.major = ggplot2::element_line(color = "#e5e7eb54", linetype = 2, linewidth = 0.1),
+        panel.grid.minor = ggplot2::element_line(color = "#e5e7eb54", linetype = 2, linewidth = 0.1),
+        plot.background = ggplot2::element_rect(fill = "#030712", linewidth = 0),
+        plot.caption = ggplot2::element_text(color = "#d1d5db", size = 8),
+        plot.margin = ggplot2::margin(5, 15, 5, 10),
+        plot.subtitle = ggplot2::element_text(color = "#d1d5db", size = 10, margin = ggplot2::margin(0, 0, 10, 0)),
+        plot.title = ggplot2::element_text(color = "#fff", face = "bold", size = 14, margin = ggplot2::margin(5, 0, 5, 0)),
+        strip.background = ggplot2::element_rect(fill = "#1d1f27", color = "#e5e7eb91", linewidth = 0.1),
+        strip.text = ggplot2::element_text(color = "#fff")
     )
 }
 
