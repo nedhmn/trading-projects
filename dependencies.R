1f846f6fd0ca14437b9b3539d1c102180974d605
nedhmn <hermannned@gmail.com>
2024-12-15 04:30:33 -0500

    fix: removed box, weird with rmarkdown build

--- dependencies.R
+++ dependencies.R
@@ -3,13 +3,10 @@
 # Description: Global dependencies
 # ---------------------------------------------------------------------------- #
 
-library(box)
 library(gt)
 library(httpgd)
 library(languageserver)
-library(pandoc)
 library(rmarkdown)
 library(tidyverse)
 library(viridis)
 library(webshot2)
-library(kableExtra)
